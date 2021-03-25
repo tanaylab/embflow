c165c13e3a7dc486043e1acf5ae7274f8c07cb38
Markus Mittnenzweig <mittnenm@n100.mcl4.weizmann.ac.il>
2021-03-25 23:31:30 +0200

    changed typo

--- scripts/test_cmp_network_flow_model.r
+++ scripts/test_cmp_network_flow_model.r
@@ -4,9 +4,10 @@ source("scripts/generate_mc_mgraph_network/gen_network.r")
 test_network_flow_model = function() {
 
     build_sing_emb_wt10_network(net_id = "test")
-    meesage("computed network flow model")
+    message("computed network flow model")
     mct1 = scdb_mctnetwork("sing_emb_wt10")
     mct2 = scdb_mctnetwork("test")
 
-    return(identical(mct1@network,mct2@network))
+    return(paste0("Recomputed network flow model is identical with sing_emb_wt10: ",
+           identical(mct1@network,mct2@network)))
 }
